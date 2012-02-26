5f14a5e610370902d2088459a73cd16304d470a7
Guillaume Tardif <guillaume.tardif@gmail.com>
2012-02-26 16:27:06 +0100

    removed debug traces

--- src/main/java/gtardif/MyController.java
+++ src/main/java/gtardif/MyController.java
@@ -7,15 +7,10 @@ import org.zdevra.guice.mvc.annotations.View;
 
 @Controller
 public class MyController {
-	public MyController() {
-		System.out.println("controller");
-	}
-
 	@Path("/hello.html")
 	@Model("msg")
 	@View("hello.jsp")
 	public String hello() {
-		System.out.println("render");
 		return "world";
 	}
 }
