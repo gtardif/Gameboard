7cc7d09891017da6594519e8d4f8257948aefed0
Guillaume Tardif <guillaume.tardif@gmail.com>
2012-02-26 17:40:32 +0100

    added push script

---
+++ push.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+function alert_user {
+	echo "${1}"
+	if [ ! -z `which growlnotify` ]; then
+		growlnotify `basename $0` -m "${1}"
+	fi	
+}
+
+function exit_ko {
+	alert_user "${1}"; exit 1
+}
+
+function exit_ok {
+	alert_user "${1}"; exit 0
+}
+
+PROJECT_DIR=$(pwd)
+BRANCH=$(git branch --no-color | awk '$1=="*" {print $2}')
+ORIGIN=$(git remote -v | awk '$1=="origin" && $3=="(push)" {print $2}')
+
+git fetch
+git add -A; git ls-files --deleted -z | xargs -0 -I {} git rm {}; git commit -m "wip"
+git rebase origin/${BRANCH}
+
+if [ "$?" -ne 0 ]
+then
+	git rebase --abort
+	git log -n 1 | grep -q -c "wip" && git reset HEAD~1
+	exit_ko "Unable to rebase. please pull or rebase and fix conflicts manually."
+fi
+git log -n 1 | grep -q -c "wip" && git reset HEAD~1
+
+rm -Rf ../privatebuild
+git clone -slb "${BRANCH}" . ../privatebuild
+cd ../privatebuild
+
+export MAVEN_OPTS="-Xmx2G -XX:MaxPermSize=1G"
+mvn -T4 verify
+BUILD_RESULT=$?
+
+if [ "$1" == "no-push" ]; then
+	alert_user "Not publishing, as per the no-push option"; exit 0
+fi
+
+if [ $BUILD_RESULT -ne 0 ]; then
+	exit_ko "Unable to build"
+fi
+
+git push $ORIGIN $BRANCH
+if [ $? -ne 0 ]; then
+	exit_ko "Unable to push"
+fi
+
+cd ${PROJECT_DIR} && git fetch
+exit_ok "Yet another successful push!"
