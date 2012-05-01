5955d7a8db28719cd27a5079013745ca9bfd52f3
Guillaume Tardif <guillaume.tardif@gmail.com>
2012-05-01 19:20:16 +0200

    empty gameList before adding lines in it

--- src/test/js/testHomePage.js
+++ src/test/js/testHomePage.js
@@ -12,9 +12,8 @@ test("page has title", function (done) {
 
 test("page can display game list", function (done) {
     Browser.visit(home, function (e, browser) {
-    	console.log("DIV : " + browser.query("#gameList"));
-        expect(browser.query("#gameList .WAITING:contains('first')")).to.be.ok();
-        expect(browser.query("#gameList .ready:contains('second')")).to.be.ok();
+        expect(browser.text("#gameList .WAITING")).to.be("first");
+        expect(browser.text("#gameList .ready")).to.be("second");
         done();
     });
 });
