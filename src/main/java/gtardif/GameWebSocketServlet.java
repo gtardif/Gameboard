6da94667bae3dfb052676bd14cf6529e3e4550fd
Guillaume Tardif <guillaume.tardif@gmail.com>
2012-05-02 16:04:06 +0200

    test including web socket working + better ws url creation

---
+++ src/main/java/gtardif/GameWebSocketServlet.java
@@ -0,0 +1,48 @@
+package gtardif;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
+
+public class GameWebSocketServlet extends WebSocketServlet {
+	private static final long serialVersionUID = 1L;
+	private final Set<GameWebSocket> webSockets = new CopyOnWriteArraySet<GameWebSocket>();
+
+	@Override
+	public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
+		System.out.println("REQUEST " + protocol);
+		return new GameWebSocket();
+	}
+
+	private class GameWebSocket implements WebSocket.OnTextMessage {
+		volatile Connection connection;
+
+		@Override
+		public void onOpen(Connection connection) {
+			this.connection = connection;
+			webSockets.add(this);
+		}
+
+		@Override
+		public void onClose(int closeCode, String message) {
+			webSockets.remove(this);
+		}
+
+		@Override
+		public void onMessage(String message) {
+			System.out.println("Message coté serveur : " + message);
+			for (GameWebSocket webSocket : webSockets) {
+				try {
+					webSocket.connection.sendMessage(message);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
--- src/test/js/testHomePage.js
+++ src/test/js/testHomePage.js
@@ -3,18 +3,43 @@ expect = require("expect.js");
 
 home = "http://localhost:" + process.env.PORT + "/games.html";
 
-test("page has title", function (done) {
-    Browser.visit(home, function (e, browser) {
-        expect(browser.text("title")).to.be("Games");
-        done();
-    });
+test("page has title", function(done) {
+	Browser.visit(home, function(e, browser) {
+		expect(browser.text("title")).to.be("Games");
+		done();
+	});
 });
 
-test("page can display game list", function (done) {
-    Browser.visit(home, function (e, browser) {
-        expect(browser.text("#gameList .WAITING")).to.be("first");
-        expect(browser.text("#gameList .ready")).to.be("second");
-        done();
-    });
+test("page can display game list", function(done) {
+	Browser.visit(home, function(e, browser) {
+		console.log("errors : " + browser.errors);
+		expect(browser.text("#gameList #first.WAITING")).to.be("first join");
+		expect(browser.text("#gameList .ready[id='second']")).to
+				.be("second join");
+		done();
+	});
 });
 
+messageReceived = function(window) {
+	console.log(window.document.querySelector("#messages"));
+	return window.document.querySelector("#messages li");
+}
+
+test("user can join game", function(done) {
+	Browser.visit(home, function(e, browser) {
+		browser.clickLink("#first a", function() {
+		});
+		browser.onalert(function(message) {
+			console.log("alert " + message);
+		});
+		console.log("errors : ");
+		console.log(browser.errors);
+
+		setTimeout(function() {
+			console.log("errors : ");
+			console.log(browser.errors);
+			expect(browser.html("#messages")).to.contain("join");
+			done();
+		}, 500);
+	});
+});
